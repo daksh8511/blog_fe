f62dd4f9064ee7c4a084e6225c73667fff7a85d3
Daksh03 <daxsathwara102@gmail.com>
2026-04-28 11:24:10 +0530

    signin fuction add

--- src/pages/Signin/Signin.tsx
+++ src/pages/Signin/Signin.tsx
@@ -1,31 +1,73 @@
 import { useNavigate } from "react-router-dom";
 import { Button } from "../../components/ui/button";
-import { Card, CardAction, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "../../components/ui/card";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "../../components/ui/card";
 import { Input } from "../../components/ui/input";
 import { Label } from "../../components/ui/label";
+import { useFormik } from "formik";
+import * as yup from 'yup'
+import api from "../../utils/Interceptor";
+import { toast } from "sonner";
 
 const Signin = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
+
+  const formik = useFormik({
+    initialValues : {
+      email : '',
+      password : ''
+    },
+    validationSchema : yup.object({
+      email : yup.string().required('Email are required'),
+      password : yup.string().required('Password are required')
+    }),
+    onSubmit : (value) => handleLogin(value)
+  })
+
+  const handleLogin = async (value) => {
+    try {
+      const response = await api.post('/signin', value)
+
+      if(response.data.success){
+        localStorage.setItem('token', response?.data?.token)
+        toast.success('Login successfully')
+        navigate('/')
+      }
+    } catch (error) {
+      console.error("Error : ",error)
+    }
+  }
   return (
     <Card className="w-full max-w-lg m-auto lg:mt-100">
-        <h2 className="font-semibold text-center text-2xl my-3">Lumina</h2>
+      <h2 className="font-semibold text-center text-2xl my-3">Lumina</h2>
       <CardHeader>
         <CardTitle>Login to your account</CardTitle>
         <CardDescription>
           Enter your email below to login to your account
         </CardDescription>
         <CardAction>
-          <Button variant="link" onClick={() => navigate('/signup')}>Sign Up</Button>
+          <Button variant="link" onClick={() => navigate("/signup")}>
+            Sign Up
+          </Button>
         </CardAction>
       </CardHeader>
       <CardContent>
-        <form>
+        <form onSubmit={formik?.handleSubmit}>
           <div className="flex flex-col gap-6">
             <div className="grid gap-2">
               <Label htmlFor="email">Email</Label>
               <Input
                 id="email"
                 type="email"
+                name="email"
+                onChange={formik?.handleChange}
                 placeholder="m@example.com"
                 required
               />
@@ -40,18 +82,19 @@ const Signin = () => {
                   Forgot your password?
                 </a>
               </div>
-              <Input id="password" type="password" required />
+              <Input id="password" type="password" required name="password"  onChange={formik?.handleChange} />
             </div>
           </div>
-        </form>
-      </CardContent>
-      <CardFooter className="flex-col gap-2">
+            <CardFooter className="flex-col gap-2">
         <Button type="submit" className="w-full">
           Signin
         </Button>
       </CardFooter>
+        </form>
+      </CardContent>
+    
     </Card>
-  )
-}
+  );
+};
 
 export default Signin;
